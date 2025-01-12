08a05ec6bd4504c855d49943fbf923aef4e74a3d
sourav <codexsourav404@gmail.com>
2025-01-12 21:53:49 +0530

    cas

--- src/app/club/[id]/challenges-and-sdg/page.tsx
+++ src/app/club/[id]/challenges-and-sdg/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import TabList from "./_components/TabList";
+import ChallengesAndSDG from "../_components/challengesAndSDG";
 
 function page() {
   return (
     <div>
       <TabList />
+      <div className="container">
+        <ChallengesAndSDG />
+      </div>
     </div>
   );
 }
